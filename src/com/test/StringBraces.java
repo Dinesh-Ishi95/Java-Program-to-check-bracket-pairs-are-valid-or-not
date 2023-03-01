54aab967441baa67204a6a66cffdb47642da66b1
Dinesh <dineshishi95@gmail.com>
2023-03-01 11:14:37 +0530

    Java Program to check correct bracket pair

--- src/com/test/StringBraces.java
+++ src/com/test/StringBraces.java
@@ -3,36 +3,25 @@ package com.test;
 public class StringBraces {
 
 	public boolean isValid(String str) {
-		char[] ch = str.toCharArray();
 
-		for (int i = 0; i < ch.length; i++) {
-			for (int j = i + 1; j < ch.length; j++) {
-				if (ch[i] == '(' && ch[j] == ')' && ch.length % 2 == 0) {
-					return true;
-				} else if (ch[i] == '{' && ch[j] == '}' && ch.length % 2 == 0) {
-					return true;
-				} else if (ch[i] == '[' && ch[j] == ']' && ch.length % 2 == 0) {
-					return true;
-				}
-				
+		char[] cs = str.toCharArray();
+		for (int i = 0; i < cs.length; i++) {
+			if (cs.length % 2 != 0) {
+				return false;
 			}
-
+			
+			if ((cs[i] == '[' && cs[i + 1] != ']') || (cs[i] == '(' && cs[i + 1] != ')')
+					|| (cs[i] == '{' && cs[i + 1] != '}')) {
+				return false;
+			}
 		}
-		return false;
-
-//		if (str.contains("()") || str.contains("{}") || str.contains("[]")) {
-//			return true;
-//		} else if (str.contains("(}") || str.contains("(]") || str.contains("[)") || str.contains("[}")
-//				|| str.contains("{)") || str.contains("{]")) {
-//			return false;
-//		}
-
+		return true;
 	}
 
 	public static void main(String[] args) {
-		String b = "(]";
+		String str = "(){}[]";
 		StringBraces sb = new StringBraces();
-		System.out.println(sb.isValid(b));
+		System.out.println(sb.isValid(str));
 	}
 
 }
